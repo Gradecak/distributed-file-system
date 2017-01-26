a9201ecb754cb8f0cc755ad1346770c351162e87
Marijan Gradecak <gradecam@tcd.ie>
2017-01-26 15:16:00 +0000

    expanded Token functionality to account for internal tokens

--- Token/src/Token/Generate.hs
+++ Token/src/Token/Generate.hs
@@ -1,10 +1,15 @@
-module Token.Generate (genToken) where
+module Token.Generate (genToken, genInternalToken) where
 
 import           Token
 import           Data.UUID     (toString)
 import           Data.UUID.V4  (nextRandom)
 
+-- | generate a new client token using the UUID library
 genToken :: String -> IO Token
 genToken srcIP = do
     t <- nextRandom
     return $ Token (toString t) "3600" srcIP
+
+-- | generate a new internal token (to be used for inter service communication)
+genInternalToken :: IO InternalToken
+genInternalToken = fmap toString nextRandom
--- Token/src/Token/Token.hs
+++ Token/src/Token/Token.hs
@@ -1,12 +1,21 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric  #-}
 
-module Token (Token(..)) where
+module Token (Token(..), InternalToken) where
 
 import           Data.Aeson
 import           GHC.Generics
 
+-- | Client tokens contain the randomly generated string,
+-- | the expiry time of the token (in seconds) and the source ip
+-- | that the token is valid for. Meaning that the token can only be used
+-- | on the machine that requested it, or though some naughty ip spoofing
 data Token = Token { token    :: String
                    , expiry   :: String
                    , sourceIp :: String
                    } deriving (Show, Eq, Ord, Generic, FromJSON, ToJSON)
+
+-- | internal tokens have no expiry as they last for the duration of the
+-- | auth server up time.
+-- |
+type InternalToken = String
